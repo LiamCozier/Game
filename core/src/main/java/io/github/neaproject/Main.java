6572386ba8721e9ca5caee92e40190ee0b9d5ea6
LiamCozier <19cozier.l@isaacnewtonsixthform.co.uk>
2025-10-08 12:03:09 +0100

    init

---
+++ core/src/main/java/io/github/neaproject/Main.java
@@ -0,0 +1,25 @@
+package io.github.neaproject;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Main extends ApplicationAdapter {
+
+
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public void render() {
+
+    }
+
+    @Override
+    public void dispose() {
+    }
+}
