c1b4ffd0a0fd898bcd31e32211d2344f3a819edd
Liam <19cozier.l@isaacnewtonsixthform.co.uk>
2025-11-08 13:25:10 +0000

    SAT working with minpendepth and collision normal

--- core/src/main/java/io/github/neaproject/Main.java
+++ core/src/main/java/io/github/neaproject/Main.java
@@ -3,66 +3,74 @@ package io.github.neaproject;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ScreenUtils;
+import io.github.neaproject.physics.Polygon;
 import io.github.neaproject.physics.shape.BoxShape;
 import io.github.neaproject.physics.PhysicsManager;
 import io.github.neaproject.physics.RigidBody;
-import io.github.neaproject.physics.shape.PolygonShape;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
 
     OrthographicCamera camera;
     ShapeRenderer sr;
-    RigidBody box;
+    RigidBody box1;
+    RigidBody box2;
 
     @Override
     public void create() {
 
-        float height = 20;
+        float height = 10;
         float ppu = Gdx.graphics.getHeight() / height;
         float width = Gdx.graphics.getWidth() / ppu;
         camera = new OrthographicCamera(width, height);
 
         sr = new ShapeRenderer();
 
-        box = new RigidBody(
-            new Vector2(6, 9),
+        box1 = new RigidBody(
+            new Vector2(-2.75f, 5),
             Vector2.Zero.cpy(),
             new BoxShape(5, 5),
-            (float)Math.PI*0.25f,
-            (float)Math.PI*0.25f,
+            (float)Math.PI*0f,
+            (float)Math.PI*1f,
             1
         );
 
-        Vector2[] verts = box.get_polygon().vertices();
-        System.out.print("P=");
-        for (int i=0; i<verts.length; i++) {
-            if (Math.abs(verts[i].x)<0.0000001) verts[i].x = 0;
-            if (Math.abs(verts[i].y)<0.0000001) verts[i].y = 0;
-            System.out.print(verts[i].toString());
-            if (i+1!=verts.length) System.out.print(",");
-        }
-        System.out.println();
-
-        PhysicsManager.get_polygon_normals(box.get_polygon());
-
+        box2 = new RigidBody(
+            new Vector2(2.75f, 5),
+            Vector2.Zero.cpy(),
+            new BoxShape(5, 5),
+            (float)Math.PI*0f,
+            (float)Math.PI*0f,
+            1
+        );
     }
 
     @Override
     public void render() {
-        float deltaT = Gdx.graphics.getDeltaTime();
-        box.physics_tick(deltaT);
+        float deltaT = Gdx.graphics.getDeltaTime() / 10f;
+        box1.physics_tick(deltaT);
+        box2.physics_tick(deltaT);
 
         camera_input();
 
         ScreenUtils.clear(0.18f, 0.24f, 0.29f, 1);
         sr.begin(ShapeRenderer.ShapeType.Line);
         sr.setProjectionMatrix(camera.combined);
-        sr.polygon(box.get_polygon().get_float_array());
+
+        Polygon p1 = box1.get_polygon();
+        Polygon p2 = box2.get_polygon();
+
+        sr.setColor(Color.WHITE);
+        if (PhysicsManager.SAT_overlap(p1, p2)) sr.setColor(Color.GREEN);
+
+        sr.polygon(box1.get_polygon().get_float_array());
+        sr.polygon(box2.get_polygon().get_float_array());
+
         sr.end();
 
 
--- core/src/main/java/io/github/neaproject/physics/PhysicsManager.java
+++ core/src/main/java/io/github/neaproject/physics/PhysicsManager.java
@@ -2,8 +2,6 @@ package io.github.neaproject.physics;
 
 import com.badlogic.gdx.math.Vector2;
 
-import java.util.Arrays;
-
 public class PhysicsManager {
 
     private static boolean AABB_overlap(BoundingBox b1, BoundingBox b2) {
@@ -21,13 +19,9 @@ public class PhysicsManager {
         int edge_count = vertices.length;
         Vector2[] edges = new Vector2[edge_count];
 
-        System.out.print("E=");
         for (int i=0; i<edge_count; i++) {
             edges[i] = vertices[(i+1)%edge_count].cpy().sub(vertices[i]);
-            System.out.print(edges[i].toString());
-            if (i+1!=edge_count) System.out.print(",");
         }
-        System.out.println();
 
         return edges;
     }
@@ -54,12 +48,44 @@ public class PhysicsManager {
         return normals;
     }
 
-    public static boolean ranges_overlap(float[] r1, float[] r2) {
-        return r1[1] >= r2[0] && r2[1] >= r1[0];
+    public static float[] project_polygon_onto_axis(Polygon p, Vector2 axis) {
+        Vector2[] vertices = p.vertices();
+        float min_projection = Float.MAX_VALUE;
+        float max_projection = -Float.MAX_VALUE;
+
+        for (int i=0; i<vertices.length; i++) {
+            float projection = axis.dot(vertices[i]);
+            if (projection < min_projection) min_projection = projection;
+            if (projection > max_projection) max_projection = projection;
+        }
+
+        return new float[]{min_projection, max_projection};
+    }
+
+    private static float get_range_overlap(float[] r1, float[] r2) {
+        float overlap = Math.min(r1[1], r2[1]) - Math.max(r1[0], r2[0]);
+        // return 0 if the overlap value is negative (no overlap)
+        return Math.max(overlap, 0f);
     }
 
     public static boolean SAT_overlap(Polygon p1, Polygon p2) {
-        Vector2[] axis = get_polygon_normals(p1, p2);
+        float min_penetration = Float.MAX_VALUE;
+        Vector2 collision_normal = null;
+
+        Vector2[] axes = get_polygon_normals(p1, p2);
+        for (Vector2 axis: axes) {
+            float[] range1 = project_polygon_onto_axis(p1, axis);
+            float[] range2 = project_polygon_onto_axis(p2, axis);
+
+            float overlap = get_range_overlap(range1, range2);
+            if (overlap < 1e-6f) {
+                return false;
+            } else if (overlap < min_penetration) {
+                min_penetration = overlap;
+                collision_normal = axis.cpy();
+            }
+        }
+
         return true;
     }
 
